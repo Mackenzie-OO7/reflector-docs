693111033867fb85d76a59dfdfda551b48536c53
Mackenzie-OO7 <levaiagbara@gmail.com>
2025-01-31 12:44:06 +0100

    switch to typescript

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from 'vitepress';
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
@@ -36,29 +36,29 @@ export default defineConfig({
     },
 
     editLink: {
-      pattern: 'https://github.com/Mackenzie-OO7/reflector-docs.git',
+      pattern: 'https://github.com/Mackenzie-OO7/reflector-docs/edit/main/docs/:path',
       text: 'Edit this page on GitHub',
     },
 
     logo: {
       alt: "Reflector Logo",
-      light: "../public/reflector-logo-light.svg",
-      dark: "../public/reflector-logo-dark.svg",
+      light: "/reflector-logo-light.svg",
+      dark: "/reflector-logo-dark.svg",
     },
 
     sidebar: [
       {
         text: 'Overview',
         items: [
-          { text: 'Getting Started', link: '../overview/getting-started' },
-          { text: 'How it works', link: '../overview/how-it-works' },
-          { text: 'Contract Interface', link: '../overview/contract-interface' },
+          { text: 'Getting Started', link: '/overview/getting-started' },
+          { text: 'How it works', link: '/overview/how-it-works' },
+          { text: 'Contract Interface', link: '/overview/contract-interface' },
           {
-            text: 'Usage Examples', link: '../overview/usage-examples',
+            text: 'Usage Examples',
             items: [
-              { text: 'Forced Position Liquidation', link: '../overview/forced-position-liquidation' },
-              { text: 'Portfolio Rebalancing', link: '../overview/portfolio-rebalancing' },
-              { text: 'Algorithmic Stablecoin', link: '../overview/algorithmic-stablecoin' },
+              { text: 'Forced Position Liquidation', link: '/overview/forced-position-liquidation' },
+              { text: 'Portfolio Rebalancing', link: '/overview/portfolio-rebalancing' },
+              { text: 'Algorithmic Stablecoin', link: '/overview/algorithmic-stablecoin' },
             ]
           },
         ],
@@ -68,16 +68,16 @@ export default defineConfig({
         items: [
           {
             text: 'How to integrate Oracles into your smart contract',
-            link: '../how-to-guides/integrate-oracles'
+            link: '/how-to-guides/integrate-oracles'
           },
         ]
       },
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/Mackenzie-OO7/reflector-docs.git' },
+      { icon: 'github', link: 'https://github.com/Mackenzie-OO7/reflector-docs' },
       { icon: 'discord', link: 'https://discord.gg/v2ggfDty2d' },
       { icon: 'twitter', link: 'https://twitter.com/in_reflector' },
     ]
   }
-})
+});
